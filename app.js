c0939bf81a58052163b6d38b0bd47f4960a353a6
Claire Lovisa <lovisa.claire@gmail.com>
2019-08-06 16:36:16 +0200

    Clean up

--- app.js
+++ app.js
@@ -2,47 +2,24 @@ import { app, errorHandler } from 'mu';
 import { fetchReportsByStatus, updateReportStatus,
          STATUS_PACKAGED, STATUS_DELIVERING, STATUS_FAILED, STATUS_DELIVERED} from './queries';
 import { deliver } from './delivery';
+import { waitForDatabase } from './database';
 import { CronJob } from 'cron';
 
 const cronFrequency = process.env.PACKAGE_CRON_PATTERN || '*/30 * * * * *';
 const hoursDeliveringTimeout = process.env.HOURS_DELIVERING_TIMEOUT || 3;
-const muSparqlEndpoint = process.env.MU_SPARQL_ENDPOINT;
 
-const rp = require('request-promise');
+waitForDatabase(createCronJob);
 
 app.get('/', async function( req, res ) {
   res.send('Hello from deliver-bbcdr-rapporten-service');
 });
 
-new CronJob(cronFrequency, async function() {
-  console.log(`BBCDR delivery triggered by cron job at ${new Date().toISOString()}`);
-  await waitForDatabase();
-  deliverPackages();
-}, null, true);
-
-const isDatabaseUp = async function() {
-  const res = await rp(muSparqlEndpoint)
-    .then(function (htmlString) {
-      return true;
-    })
-    .catch(function (err) {
-      return false;
-    });
-  return res;
-};
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function createCronJob() {
+  return new CronJob(cronFrequency, function() {
+    console.log(`BBCDR delivery triggered by cron job at ${new Date().toISOString()}`);
+    deliverPackages();
+  }, null, true);
 }
-
-const waitForDatabase = async function() {
-  let loop = true;
-  while (loop) {
-    console.log("Waiting for database... ");
-    loop = !(await isDatabaseUp());
-    await sleep(2000);
-  }
-};
 
 const deliverPackages = async function(){
   try {
